5ea6b6b68594af87b3b348cb711dc246967aa91b
Ryan <thisianemailaddressthatis@gmail.com>
2022-10-26 18:26:03 -0500

    Added asset caching to service worker

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,20 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener("click", async () => {
+  event.prompt();
+  installBtn.setAttribute("disabled", true);
+  installBtn.textContent = "Installed!";
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener("appinstalled", (event) => {
+  textHeader.textContent = "Successfully installed!";
+  console.log("👍", "appinstalled", event);
+});
